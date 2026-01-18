b9b1e6b625d79ac0bd7e70baaff744b097ddf451
Ibrahim <hr@sambo.sa>
2026-01-18 05:11:53 +0200

    Refactor: Move plant data to js file and add local images

---
+++ js/plant_data.js
@@ -0,0 +1,12 @@
+const plants = [
+    { n: "Tomato", a: "طماطم", m: 45, i: "assets/images/tomato.png" },
+    { n: "Cucumber", a: "خيار", m: 72, i: "assets/images/cucumber.png" },
+    { n: "Arugula", a: "جرجير", m: 80, i: "assets/images/arugula.png" },
+    { n: "Carrot", a: "جزر", m: 35, i: "assets/images/carrot.png" },
+    { n: "Lettuce", a: "خس", m: 60, i: "assets/images/lettuce.png" },
+    { n: "Pepper", a: "فلفل", m: 55, i: "assets/images/pepper.png" },
+    { n: "Spinach", a: "سبانخ", m: 85, i: "assets/images/spinach.png" },
+    { n: "Beans", a: "فاصوليا", m: 20, i: "assets/images/beans.png" },
+    { n: "Peas", a: "بازلاء", m: 90, i: "assets/images/peas.png" },
+    { n: "Cabbage", a: "كرنب", m: 50, i: "assets/images/cabbage.png" }
+];
